92d10039fafd4581de3d4e8a31348cd5cf1b157b
ahka07001 <ahka07001@E-PW05CGQT>
2024-10-03 14:14:41 +0200

    Add project files.

---
+++ Inlämnings uppgift 2.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace inlämnings_._uppgift_._2_._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur dagar ska du använda bilen?");
+            int dagar= int.Parse(Console.ReadLine());
+            int Dag1 = 300;
+            int Övrigadagar = 500;
+            Console.WriteLine("Hur många kilometer ska du köra bilen?");
+            int kilometer= int.Parse(Console.ReadLine());
+            int sum= Dag1 + dagar*Övrigadagar + kilometer;
+            Console.WriteLine(sum);
+
+
+
+
+
+        }
+    }
+}
